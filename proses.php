280e095bde643e66680f890312e21095099a3a3c
auliahasna929 <42725478+auliahasna929@users.noreply.github.com>
2018-09-24 10:54:17 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,18 @@
 <?php
+$hobi = $_POST['hobi'];
+echo "Hobi yang disukai anda :";
+foreach ($hobi as $key => $value) {
+	
+}
+{
+	print_r($_POST['hobi']);
+}
 
+$file = $_FILES['dokumen'];
+$nama_file = $file['name'];
+$nama_tmp = $file['tmp_name'];
+$upload_dir ="upload/";
+move_uploaded_file($nama_tmp,$upload_dir.$nama_file);
+echo "file berhasil diupload. <br>"
 ?>
+<img src="upload/<?php echo $nama_file; ?>" width="100px">
